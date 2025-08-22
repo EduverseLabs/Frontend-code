0edbb6a3d3d445f8a391de5a48727ed3a6b87143
ayomide akinrata <akinrataa@gmail.com>
2025-08-22 13:05:06 +0100

    added eduverce lab when feed is clicked

--- src/DashBoard.tsx
+++ src/DashBoard.tsx
@@ -1,9 +1,10 @@
 import { AiFillBell } from 'react-icons/ai';
 import { BsDot } from 'react-icons/bs';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 import { IoMenuOutline } from 'react-icons/io5';
 import React, { useState, useRef, useEffect } from 'react';
 import Notification from './pages/DashBoardPages/Notification.tsx';
+
 
 
 const sideMenuData = [
@@ -20,10 +21,10 @@ const sideMenuData = [
 ]
 
 const DashBoard = () => {
+    const location = useLocation();
     const [openNotification, setOpenNotification] = useState(false);
-    // const [openMenu, setOpenMenu] = useState(false);
     const dropdownRef = useRef(null);
-    // Close dropdown when clicking outside
+
     useEffect(() => {
         const handleClickOutside = (e: MouseEvent) => {
             if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
@@ -33,34 +34,38 @@ const DashBoard = () => {
         document.addEventListener("mousedown", handleClickOutside);
         return () => document.removeEventListener("mousedown", handleClickOutside);
     }, []);
+    let showHeading = location.pathname === '/feed' ? 'block' : 'hidden';
     return (
         <>
             <div className=' flex h-screen'>
+                {/* menu section */}
                 <div className="bg-blue-600 w-2/12 p-2  h-screen text-white  hidden lg:flex lg:flex-row flex-col">
-                    <ul className="flex flex-col gap-2 my-6 w-full">
-                        {sideMenuData.map((item, index) => (
-                            <Link to={item.path} className="flex items-center">
-                                <li key={index} className="text-white p-2 hover:bg-white hover:text-black cursor-pointer w-full rounded-md transition-colors">
+                    <div className=' w-full h-full '>
+                        <h1 className={`px-2 pt-6 text-2xl font-bold ${showHeading}`}>EduverseLabs</h1>
+                        <ul className=" gap-2  my-6 w-full font-semibold">
+                            {sideMenuData.map((item, index) => (
+                                <Link to={item.path} className=" ">
+                                    <li key={index} className="text-white  p-2 hover:bg-white hover:text-black cursor-pointer w-full rounded-md transition-colors">
+                                        <span className="text-lg">{item.name}</span>
+                                    </li>
+                                </Link>
+                            ))}
 
-                                    <span className="text-lg">{item.name}</span>
-
-                                </li>
-                            </Link>
-
-                        ))}
-
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
+
                 <div className="w-full lg:w-10/12 h-screen">
-                    <div className=" px-2 lg:px-8 mb-2 lg:my-4 border-b-[1px] border-gray-300 flex  lg:justify-between items-center lg:h-16 bg-blue-700 text-white lg:bg-white lg:text-black ">
+                    {/* header section */}
+                    <div className=" px-2 lg:px-7 mb-2 lg:my-4 border-b-[1px] border-gray-300 flex  lg:justify-between items-center lg:h-16 bg-blue-700 text-white lg:bg-white lg:text-black ">
 
                         <div className='my-2 w-full flex  h-full items-center '>
                             <div className='lg:hidden p-2 font-bold text-white' onClick={() => alert("Menu clicked!")}>
                                 <IoMenuOutline size={30} />
                             </div>
-                            <div className='flex flex-col lg:flex-row  p-1'>
+                            <div className='flex flex-col lg:flex-row lg:items-center w-full lg:w-auto  '>
                                 <h1 className='lg:hidden text-xl font-bold'>EduVerse</h1>
-                                <div>
+                                <div className='w-full'>
                                     <h1 className='font-bold text-md capitalize lg:hidden flex'>Welcome Back Rolake</h1>
                                     <h1 className='font-bold text-2xl capitalize hidden lg:flex '>Welcome back, <span className='text-blue-600'>Rolake</span> !</h1>
                                     <h5 className='text-[11px]'>Here is what is happening with your studies today.</h5>
@@ -69,7 +74,11 @@ const DashBoard = () => {
                         </div>
                         {/* // <img src={eduverseLabsLogo} alt="Eduverse Labs Logo" className='max-w-20' />  */}
                         <div className='flex items-center  '>
-                            <div className='p-4 lg:p-2 cursor-pointer' onClick={() => setOpenNotification((prev) => (!prev))} ><AiFillBell size={25} /></div>
+                            <div className='p-4 lg:p-2 cursor-pointer' onClick={() => setOpenNotification((prev) => (!prev))} >
+
+                                <p className='relative'><AiFillBell size={25} /></p>
+                                {/* <p className='absolute top-5 right-[205px] text-md w-10 h-10  text-white justify-center'>3</p> */}
+                            </div>
                             <div className='items-center  text-black px-1 mr-10  h-10 p-4 rounded-lg border border-black hidden lg:flex'>
                                 <p className='px-4 py-2  '>Rolake</p>
                                 <p className='  text-2xl text-[#0CF37F]'><BsDot size={35} />
@@ -80,6 +89,7 @@ const DashBoard = () => {
                     </div>
                     <Outlet />
                     <div className=''>
+                        {/* Notification dropdown */}
                         <div className={`absolute right-0 -top-4 w-full h-full md:w-1/2 lg:w-1/3  rounded-lg shadow-lg border border-gray-200 z-50 bg-white ${openNotification ? 'block' : 'hidden'}`} ref={dropdownRef}>
                             <Notification />
                         </div>
